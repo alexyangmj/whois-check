96d21bf68b43ea7a274788b16b0fced9bfc22501
Alex Yang <Undisclosed@localhost>
2024-04-15 23:16:17 +0800

    v2.0b

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Return domain registrar info and resolved IP (or Cannonical Name) using WHOIS DB
 The binary for MacOS (compiled on Sonoma 14.x) is included in this repository.
 
 ```
-whois-check v2.0a
+whois-check v2.0b
 Last Update: 15 Apr 2024, Alex Yang (https://linkedin.com/in/4yang)
 
 Usage for Single IP query:
--- whois-check.go
+++ whois-check.go
@@ -21,7 +21,7 @@ func IsIpv6Net(host string) bool {
 
 func main() {
     
-    Banner := "whois-check v2.0a\n"
+    Banner := "whois-check v2.0b\n"
     Banner = Banner + "Last Update: 15 Apr 2024, Alex Yang (https://linkedin.com/in/4yang)\n\n"
     Banner = Banner + "Usage for Single IP query:\n"
     Banner = Banner + "    whois-check [ipv4 | ipv6 | domain.com]\n\n"
@@ -206,7 +206,10 @@ func main() {
         return
     }
     
-    ip, _ := net.ResolveIPAddr("ip4", input)
+    ip, err := net.ResolveIPAddr("ip4", input)
+    if err != nil {
+        fmt.Println ("Error: ", err)
+    }
     fmt.Println("Resolved IP: \t\t\t", ip)
         
     switch Switch {
